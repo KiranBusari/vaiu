bd26422aaafb2108d0af7f56502da86788a27ad5
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-11-03 23:56:04 +0530

    fix layout and loaders and error pages

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import { NuqsAdapter } from "nuqs/adapters/next";
+import { QueryProvider } from "@/components/query-provider";
+import { Toaster } from "@/components/ui/sonner";
+import { cn } from "@/lib/utils";
+
+import "./globals.css";
+import { ThemeProvider } from "./theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jira Clone",
+  description:
+    "Plan, track, and manage your agile and software development projects in Jira. Customize your workflow, collaborate, and release great software",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn(inter.className, "antialiased min-h-screen")}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NuqsAdapter>
+            <QueryProvider>
+              <Toaster richColors theme="light" />
+              {children}
+            </QueryProvider>
+          </NuqsAdapter>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
