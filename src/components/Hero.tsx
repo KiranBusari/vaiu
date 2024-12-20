79c6e2cc5d32e14471a9b390ff6717afa7888ef3
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-12-20 19:37:01 +0530

    Refactor Hero and LampContainer components for improved styling and responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,19 +5,17 @@ import { LampContainer } from "./ui/lamp";
 
 const Hero = () => {
   return (
-    <LampContainer className="flex max-h-screen place-content-center">
-      <div className="text-center">
-        <h1 className="bg-gradient-to-b flex-col flex from-slate-50 to-slate-500 bg-clip-text text-8xl font-bold leading-[150%] text-transparent">
-          Contributions
-          <span className="bg-gradient-to-b from-blue-400 to-blue-700 bg-clip-text text-8xl font-semibold text-transparent p-4">
-            Transform your Learning
-          </span>
-        </h1>
-        <p className="text-lg text-slate-300 py-4">
-          A platform where you can contribute to real-world projects in a closed
-          environment.
-        </p>
-      </div>
+    <LampContainer className="max-h-screen flex place-content-center text-center">
+      <h1 className="bg-gradient-to-b flex-col flex from-slate-50 to-slate-800 bg-clip-text sm:text-8xl text-6xl font-bold leading-[150%] text-transparent">
+        Contributions
+        <span className="bg-gradient-to-b from-blue-500 to-blue-900 bg-clip-text sm:text-8xl text-3xl font-semibold text-transparent p-4">
+          Transform your Learning
+        </span>
+      </h1>
+      <p className="text-lg text-slate-300 py-4">
+        A platform where you can contribute to real-world projects in a closed
+        environment.
+      </p>
       <div className="mt-10">
         <Link href={"/sign-in"}>
           <Button
--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -12,7 +12,7 @@ export const LampContainer = ({
 }) => {
   return (
     <div className={cn("flex flex-col", className)}>
-      <div className="relative flex w-full min-h-screen flex-1 scale-y-125 items-center justify-center isolate z-0">
+      <div className="relative flex w-full min-h-screen flex-1 items-center justify-center isolate z-0">
         <motion.div
           initial={{ opacity: 0.5, width: "15rem" }}
           whileInView={{ opacity: 1, width: "30rem" }}
@@ -24,10 +24,10 @@ export const LampContainer = ({
           style={{
             backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
           }}
-          className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-slate-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
+          className="absolute inset-auto right-1/2 h-56 w-full max-w-xl bg-gradient-conic from-slate-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
         >
-          <div className="absolute  w-[100%] left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-          <div className="absolute  w-40 h-[100%] left-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
+          <div className="absolute w-full left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute w-40 h-full left-0 bg-slate-950 bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
         </motion.div>
         <motion.div
           initial={{ opacity: 0.5, width: "15rem" }}
@@ -40,14 +40,14 @@ export const LampContainer = ({
           style={{
             backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
           }}
-          className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-slate-500 text-white [--conic-position:from_290deg_at_center_top]"
+          className="absolute inset-auto left-1/2 h-56 w-full max-w-xl bg-gradient-conic from-transparent via-transparent to-slate-500 text-white [--conic-position:from_290deg_at_center_top]"
         >
-          <div className="absolute w-40 h-[100%] right-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
-          <div className="absolute  w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute w-40 h-full right-0 bg-slate-950 bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
+          <div className="absolute w-full right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
         </motion.div>
-        <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl"></div>
+        <div className="absolute top-1/2 h-48 max-w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl"></div>
         <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
-        <div className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-slate-500 opacity-50 blur-3xl"></div>
+        <div className="absolute inset-auto z-50 h-36 w-full max-w-lg -translate-y-1/2 rounded-full bg-slate-500 opacity-50 blur-3xl"></div>
         <motion.div
           initial={{ width: "8rem" }}
           whileInView={{ width: "16rem" }}
@@ -56,7 +56,7 @@ export const LampContainer = ({
             duration: 0.8,
             ease: "easeInOut",
           }}
-          className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-slate-400 blur-2xl"
+          className="absolute inset-auto z-30 h-36 w-full max-w-md -translate-y-[6rem] rounded-full bg-slate-400 blur-2xl"
         ></motion.div>
         <motion.div
           initial={{ width: "15rem" }}
@@ -66,10 +66,10 @@ export const LampContainer = ({
             duration: 0.8,
             ease: "easeInOut",
           }}
-          className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-slate-400"
+          className="absolute inset-auto z-50 h-0.5 w-full max-w-xl -translate-y-[7rem] bg-slate-400"
         ></motion.div>
 
-        <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-950 "></div>
+        <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-950"></div>
       </div>
 
       <div className="relative z-50 flex -translate-y-80 flex-col items-center px-5 place-content-center -mt-36">
