e324cf52ea87a72b4f8443e0cb612707a90be46a
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-12-20 22:12:40 +0530

    Enhance Hero component styling for better responsiveness and visual appeal

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,21 +5,21 @@ import { LampContainer } from "./ui/lamp";
 
 const Hero = () => {
   return (
-    <LampContainer className="max-h-screen flex place-content-center text-center">
-      <h1 className="bg-gradient-to-b flex-col flex from-slate-50 to-slate-800 bg-clip-text sm:text-8xl text-6xl font-bold leading-[150%] text-transparent">
+    <LampContainer className="sm:max-h-screen min-h-screen sm:flex sm:place-content-center text-center">
+      <h1 className="bg-gradient-to-b flex-col flex from-slate-50 to-slate-800 bg-clip-text sm:text-8xl text-5xl font-bold leading-[150%] text-transparent">
         Contributions
         <span className="bg-gradient-to-b from-blue-500 to-blue-900 bg-clip-text sm:text-8xl text-3xl font-semibold text-transparent p-4">
           Transform your Learning
         </span>
       </h1>
-      <p className="text-lg text-slate-300 py-4">
+      <p className="sm:text-lg text-sm text-slate-300 py-4">
         A platform where you can contribute to real-world projects in a closed
         environment.
       </p>
       <div className="mt-10">
         <Link href={"/sign-in"}>
           <Button
-            className="text-blue-400 hover:text-blue-500 font-bold rounded-full"
+            className="text-slate-100 hover:text-slate-200 font-bold rounded-full outline-blue-600 outline"
             variant={"outline"}
             size={"lg"}
           >
