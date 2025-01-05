4f4cab911e501cfd5510019e82f6c7045fe22a9f
Akshai-M <mylapuramakshaikumar@gmail.com>
2025-01-05 12:09:36 +0530

    Landing

---
+++ src/components/Landing/Landing.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import  Hero  from '@/components/Hero'
+import Pricing  from '@/components/Landing/Pricing'
+
+export default  function Landing() {
+  return (
+    < >
+      <Hero/>
+      <Pricing/>
+    </>
+  )
+}
+
+
