e6cb2409f937fb40021f3986857ea8d186416f1b
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2025-02-19 01:12:43 +0530

    feat: integrate Navbar component into Home page and enhance Hero section styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,15 @@ import { getCurrent } from "@/features/auth/queries";
 
 import Hero from "@/components/Hero";
 import { getWorkspaces } from "@/features/workspaces/queries";
+import { Navbar } from "@/components/mainNavbar";
 
 export default async function Home() {
   const current = await getCurrent();
 
   if (!current) {
     return (
-      <div>
-        <div className="grid-background"></div>
+      <div className="container mx-auto">
+        <Navbar />
         <Hero />
       </div>
     );
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,28 +1,27 @@
 import React from "react";
 import { Spotlight } from "./ui/spotlight";
-import { Navbar } from "./mainNavbar";
 import Link from "next/link";
 import { Button } from "./ui/button";
 
 const Hero = () => {
   return (
     <div className="min-h-screen">
-      <Navbar />
       <div className="mx-auto flex min-h-[80vh] max-w-7xl flex-col items-center justify-center px-4 sm:px-6 lg:px-8">
         <Spotlight
           className="sm:-top-40 top-44 left-0 md:left-60 md:top-10"
           fill="blue"
         />
         <div className="text-center leading-8">
-          <h1 className="bg-gradient-to-b from-neutral-400 to-neutral-800 dark:from-neutral-200 dark:to-neutral-500 bg-clip-text text-[40px] sm:text-5xl md:text-7xl font-bold leading-[150%] tracking-tight text-transparent">
-            Transform your Learning with <br />
-            <span className="bg-gradient-to-b from-blue-400 to-blue-700 bg-clip-text sm:text-7xl md:text-9xl text-[54px] font-semibold text-transparent">
+          <h1 className="bg-gradient-to-b from-neutral-400 to-neutral-800 dark:from-neutral-200 dark:to-neutral-500 bg-clip-text text-[40px] sm:text-5xl md:text-7xl font-bold leading-[150%] tracking-wide text-transparent relative">
+            Transform your learning with <br />
+            <span className="bg-gradient-to-b text-center from-blue-400 to-blue-700 bg-clip-text sm:text-7xl md:text-9xl text-[54px] tracking-wide font-semibold text-transparent">
               Contributions
             </span>
           </h1>
-          <p className="sm:text-lg text-gray-500 text-sm">
-            A platform where you can contribute to real-world projects in a
-            closed environment.
+          <p className="sm:text-lg text-gray-400 text-sm">
+            A platform where you can contribute to{" "}
+            <br className="block md:hidden" /> real-world projects in a closed
+            environment.
           </p>
         </div>
         <div className="mt-10">
